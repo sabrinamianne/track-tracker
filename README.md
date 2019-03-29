cd7586aba09d0bedd021ca64a56cbcf536a40f04
sabrina mianne <sabrina.mianne@gmail.com>
2019-03-29 16:28:54 -0700

    I have almost done the code, I'm almost ready to merge my test branch to my master branch

---
+++ README.md
@@ -0,0 +1,41 @@
+# _Epicodus Code Review Intro to programming Week 2_
+
+#### _Create a Track Suggester using HTML, CSS, BOOTSTRAP, JQUERY, for review, 03.29.2019_
+
+#### _By**Sabrina M**_
+
+## Description
+
+_Track Suggester Review at Epicodus _
+
+## Setup/Installation Requirements
+
+* _clone this repository_
+*_open the html file with a web browser_
+*_This project is using Github pages_
+*_Link of my project https://sabrinamianne.github.io/track_tracker/
+
+
+
+## Known Bugs
+
+_ No bugs related_
+
+## Support and contact details
+
+_Contact me by e-mail at the following address : contactll@gmail.com_
+
+## Technologies Used
+
+_HTML_
+_Bootstrap_
+_CSS_
+_jQuery-3.3.1_
+
+
+
+### License
+
+*This software is licenced under the MIT licence*
+
+Copyright (c) 2019 ** Sabrina M_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $("form#track").submit(function(event) {
-    var name_user = $("input#namequestion").val();
+
+    var name_user = $("input#namequestion").val().toUpperCase();
     var age = parseInt($("input#agequestion").val());
     var answer1 = $("select#question1").val();
     var answer2 = $("select#question2").val();
@@ -10,12 +11,17 @@ $(document).ready(function() {
     var number_result = 0 ;
 
 
+
     if (age <=40 ) {
       number_result += 20
     }
 
     else if (age >= 40 ) {
       number_result += 10
+    }
+
+    else {
+      alert ("Please, enter your answers.")
     }
 
     if (answer1 === 'no') {
@@ -47,32 +53,38 @@ $(document).ready(function() {
     }
 
     if (answer4 === 'webD') {
-      number_result -= 50
+      number_result += 30
     }
 
     else if (answer4 === 'softwareD') {
-      number_result += 100
+      number_result += 80
     }
 
     if (answer5 === 'mob') {
-      number_result += 60
+      number_result -= 40
     }
 
     else if (answer5 === 'desktop') {
-      number_result += 10
+      number_result += 30
     }
 
+    if (number_result <= 110) {
+      $("#user_name").empty().append(name_user);
+      $("#track_result").empty().append("CSS/DESIGN : Your score is in the amount of "+ number_result + " , that means you prefer to focus on design and see yourself more as Web Developer.");
+      $("#result").show();
+    }
 
-alert (number_result)
+    if ((number_result > 110) && (number_result < 250)) {
+      $("#user_name").empty().append(name_user);
+      $("#track_result").empty().append("RUBY/RAILS : Your score is in the amount of "+ number_result + " , that means you prefer to focus on UX (User Experience) for mobile and see yourself more as Software Developer.");
+      $("#result").show();
+    }
 
-
-
-
-  //  $("#user_name").empty().append(name_user);
-  //  $("#track_result").empty().append("Ruby/Rails");
-//    $("#result").show();
-
-
+    if (number_result >= 250) {
+      $("#user_name").empty().append(name_user);
+      $("#track_result").empty().append("C#/.NET : Your score is in the amount of "+ number_result + " , that means you prefer to focus on Data (Back-end) and see yourself more as Software Developer at Microsoft :)");
+      $("#result").show();
+    }
 
     event.preventDefault();
   });
